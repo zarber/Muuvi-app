59d345f43352d32ed028dba93f38b4a0febe6979
niininene <103677045+niininene@users.noreply.github.com>
2023-04-13 12:27:47 +0300

    Organizing folders

--- routes/index.js
+++ routes/index.js
@@ -18,8 +18,8 @@ router.get('/etusivu_opiskelija', ensureAuth, async (req, res) => {
     const decoded = jwt.verify(req.cookies.cookieToken, process.env.SECRET);
     const stories = await Story.find({ user: decoded._id }).lean();
     // const stories = await Story.find({}).lean();
-    res.render('etusivu_opiskelija', {
-      layout: 'etusivu_opiskelija',
+    res.render('frontpage_student', {
+      layout: 'student/frontpage_student',
       // username: decoded.username,
       stories,
     });
@@ -46,40 +46,43 @@ router.get('/dashboard', ensureAuth, async (req, res) => {
   }
 });
 
-router.get('/aktiviteetit_ja_paivakirja', (req, res) => {
-  res.render('activities_and_diary', {
-    layout: 'activities_and_diary',
-  });
-});
-
-router.get('/liikuntasuunnitelma', (req, res) => {
-  res.render('excercise_plan', {
-    layout: 'excercise_plan',
-  });
-});
-
 router.get('/liikuntasuunnitelma_potilas', (req, res) => {
   res.render('student_excercise_plan', {
-    layout: 'student_excercise_plan',
+    layout: 'student/student_excercise_plan',
   });
 });
 
-router.get('/etusivu_ammattilainen', (req, res) => {
-  res.render('etusivu_ammattilainen', {
-    layout: 'etusivu_ammattilainen',
-  });
-});
-
-router.get('/potilaslista', (req, res) => {
-  res.render('patientlist', {
-    layout: 'patientlist',
+router.get('/aktiviteetit_ja_paivakirja', (req, res) => {
+  res.render('activities_and_diary', {
+    layout: 'student/activities_and_diary',
   });
 });
 
 router.get('/hrv_mittaukset', (req, res) => {
   res.render('hrv_measurements', {
-    layout: 'hrv_measurements',
+    layout: 'student/hrv_measurements',
   });
 });
+
+
+router.get('/liikuntasuunnitelma', (req, res) => {
+  res.render('excercise_plan', {
+    layout: 'nurse/excercise_plan',
+  });
+});
+
+router.get('/etusivu_ammattilainen', (req, res) => {
+  res.render('frontpage_nurse', {
+    layout: 'nurse/frontpage_nurse',
+  });
+});
+
+router.get('/potilaslista', (req, res) => {
+  res.render('patientlist', {
+    layout: 'nurse/patientlist',
+  });
+});
+
+
 
 module.exports = router;
