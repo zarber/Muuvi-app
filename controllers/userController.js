c6aface87b7a79bce4d6bdf68c6d923f4250cda5
niininene <103677045+niininene@users.noreply.github.com>
2023-04-26 11:08:13 +0300

    Patienlist, KubiosCloud testing

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,4 +37,14 @@ const signupUser = async (req, res) => {
   }
 }
 
-module.exports = { signupUser, loginUser }
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.getUsers()
+    res.status(200).json(users)
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+}
+
+
+module.exports = { signupUser, loginUser, getUsers }
