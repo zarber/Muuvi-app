31dde52c82c358ee68e31d38dbd7aa4fbb4cbdb0
Enni <ennini@metropolia.fi>
2023-05-02 19:07:02 +0300

    Adding CSS Styling for error views

--- routes/hrvResults.js
+++ routes/hrvResults.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // controller functions
-const { getHRVdata, postHRVdata } = require('../controllers/hrvDataController');
+const { getHRVdata } = require('../controllers/hrvDataController');
 
 router.get('/', getHRVdata);
 
